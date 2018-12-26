fc2e411100b127524a3d320d0d9e39fb0ede420f
sandeep <Footprintss@123>
2018-12-26 12:03:33 +0530

    Apptitude test added

---
+++ src/intjavafx/aptitude/AptitudeTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intjavafx.aptitude;
+
+/**
+ *
+ * @author sandeep
+ */
+public class AptitudeTest {
+    
+    public void test1() {
+        System.err.println("Test 1");
+    }
+    
+}
